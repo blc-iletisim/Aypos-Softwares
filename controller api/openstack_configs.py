60469af5a6ab116ffd05579c6b4324c6aca02805
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2024-11-07 14:53:49 +0300

    Create openstack_configs.py

---
+++ controller api/openstack_configs.py
@@ -0,0 +1,6 @@
+auth_url = "http://10.150.1.251:35357/v3"
+project_name = "admin"
+username = "admin"
+password = "WHMFjzLBHf1N6FxPnZpCDsXYdXewgjsvwju385Mk"
+user_domain_name = "Default"
+project_domain_name ="Default"
