f8c0145a61a0995edbb8977fac0616ccb4518011
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2025-01-22 15:07:46 +0300

    Update ansible_run.py

--- infra api demo/ansible_run.py
+++ infra api demo/ansible_run.py
@@ -1,19 +1,19 @@
 import ansible_runner
 import inspect
-from get_n_lineinv import *
+# from get_n_lineinv import *
 
-print(inspect.getargspec(ansible_runner.run))
-print(dir(ansible_runner))
+# print(inspect.getargspec(ansible_runner.run))
+# print(dir(ansible_runner))
 
 
 def ansible_runners(vm_num):
-    change_pup_inv(vm_num+1)
-    r = ansible_runner.run(playbook='/home/casper/installer/node_exporter.yml', inventory="/home/casper/installer/puppet_inventory")
+    # change_pup_inv(vm_num+1)
+    r = ansible_runner.run(playbook='/home/ubuntu/installer/node_exporter.yml', inventory="/home/casper/installer/inventory")  # /puppet_inventory")
     print("{}: {}".format(r.status, r.rc))
     # successful: 0
 
-    r = ansible_runner.run(playbook='/home/casper/installer/stress_sender.yml', inventory="/home/casper/installer/puppet_inventory")
-    print("{}: {}".format(r.status, r.rc))
+    # r = ansible_runner.run(playbook='/home/casper/installer/stress_sender.yml', inventory="/home/casper/installer/puppet_inventory")
+    # print("{}: {}".format(r.status, r.rc))
 
 
 if __name__=='__main__':
