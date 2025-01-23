a929ccc1d940b10119ffb30ba9f9dac4de6570d0
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2025-01-23 16:27:58 +0300

    Update openstack_configs.py

--- controller api/openstack_configs.py
+++ controller api/openstack_configs.py
@@ -1,6 +1,15 @@
+# openstack admin-openrc.sh file configurations
 auth_url = "http://10.150.1.251:35357/v3"
 project_name = "admin"
 username = "admin"
 password = "WHMFjzLBHf1N6FxPnZpCDsXYdXewgjsvwju385Mk"
 user_domain_name = "Default"
 project_domain_name ="Default"
+
+
+# remote process configs
+vm_ip = "10.150.1.130"
+bash_script = "/home/ubuntu/randomStressor.sh"
+pem_key_path = "/home/ubuntu/ayposKeypair.pem"
+
+
