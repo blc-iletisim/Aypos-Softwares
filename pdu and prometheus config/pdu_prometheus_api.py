5175f80ae067c7d87bb86a3a4a8b6727fd391621
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2025-01-23 16:09:38 +0300

    Update pdu_prometheus_api.py

--- pdu and prometheus config/pdu_prometheus_api.py
+++ pdu and prometheus config/pdu_prometheus_api.py
@@ -6,13 +6,22 @@ app = FastAPI()
 
 
 def split_string (data):
+    print(data)
     seperator="\""
+
     parts = data.split(seperator,1)
+    print(parts)
     return tuple(parts)
 
+def spl_int(data):
+    print(data)
+    num = float(data.split(":")[1])
+    return num
 
 def spl (data):
+    print(data)
     test = split_string(data)[1]
+    print(test)
     test2 = split_string(test)[0]
     return test2
 
@@ -21,56 +30,83 @@ import time
 
 def get_sensor_data():
     start = time.time()
-    voltage_1_12 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.3.1.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    voltage_1_12 = float(spl(voltage_1_12))
- 
-    voltage_13_24 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.3.2.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    voltage_13_24 = float(spl(voltage_13_24))
+    pdu_ip = "10.150.1.78"
 
-    curr_compute4 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.8.1.21.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    curr_compute4 = float(spl(curr_compute4))
+    # voltage_1_12 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.3.1.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # voltage_1_12 = float(spl(voltage_1_12))
 
-    curr_compute3 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.8.1.6.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    curr_compute3 = float(spl(curr_compute3))
+    # voltage_13_24 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.3.2.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # voltage_13_24 = float(spl(voltage_13_24))
+
+    # curr_compute4 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.8.1.21.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # curr_compute4 = float(spl(curr_compute4))
+
+    # curr_compute3 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.8.1.6.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # curr_compute3 = float(spl(curr_compute3))
 
     # curr_compute
-    curr_compute1 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.8.1.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    curr_compute1 = float(spl(curr_compute1))
+    # curr_compute1 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.8.1.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # curr_compute1 = float(spl(curr_compute1))
 
-    curr_compute2 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.8.1.10.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    
-    energy_1_12 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.3.1.5.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # curr_compute2 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.8.1.10.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
 
-    energy_13_24 = subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.3.2.5.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    # energy_1_12 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.3.1.5.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
 
-    curr_compute2 = float(spl(curr_compute2))
-    
-    energy_1_12 = float(spl(energy_1_12))
-    energy_13_24 = float(spl(energy_13_24))
+    # energy_13_24 = subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.3.2.5.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
 
+
+    def snmp_get(oid):
+        return subprocess.Popen([
+            r"snmpget", "-v1", "-c", "public", pdu_ip, oid
+        ], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+
+    # Retrieve voltages
+    voltage_1_12 = float(spl_int(snmp_get("iso.3.6.1.4.1.30966.5.1.1.3.5.0"))) # float(spl(snmp_get("iso.3.6.1.4.1.30966.5.1.1.3.5.0")))
+    voltage_13_24 = voltage_1_12
+    # Retrieve currents
+    curr_compute1 = float(spl(snmp_get("iso.3.6.1.4.1.30966.5.1.1.10.5.0")))  # Socket 5
+    curr_compute2 = float(spl(snmp_get("iso.3.6.1.4.1.30966.5.1.1.10.4.0")))  # Socket 4
+    curr_compute3 = float(spl(snmp_get("iso.3.6.1.4.1.30966.5.1.1.10.9.0")))  # Socket 9
+    curr_compute4 = float(spl(snmp_get("iso.3.6.1.4.1.30966.5.1.1.10.1.0")))  # Socket 1
+
+    # Retrieve energies
+    energy_1_12 = float(spl_int(snmp_get("iso.3.6.1.4.1.30966.5.1.1.3.9.0"))) # float(spl(snmp_get("iso.3.6.1.4.1.30966.5.1.1.3.9.0")))
+    energy_13_24 = energy_1_12 # float(spl(snmp_get("iso.3.6.1.4.1.30966.7.1.3.2.5.0")))
+
+    # Compute proportions and energies per component
     total_curr_1_12 = curr_compute1 + curr_compute2 + curr_compute3
-    currprop1 = curr_compute1 / total_curr_1_12 
+    currprop1 = curr_compute1 / total_curr_1_12
     currprop2 = curr_compute2 / total_curr_1_12
     currprop3 = curr_compute3 / total_curr_1_12
-    
+
+    curr_compute2 = float(curr_compute2)
+
+    energy_1_12 = float(energy_1_12)
+    energy_13_24 = float(energy_13_24)
+
+    total_curr_1_12 = curr_compute1 + curr_compute2 + curr_compute3
+    currprop1 = curr_compute1 / total_curr_1_12
+    currprop2 = curr_compute2 / total_curr_1_12
+    currprop3 = curr_compute3 / total_curr_1_12
+
     energy_comp1 = currprop1 * energy_1_12
     energy_comp2 = currprop2 * energy_1_12
     energy_comp3 = currprop3 * energy_1_12
     energy_comp4 = energy_13_24
 
-    pf_compute1 =  subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.9.4.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    pf_compute1 =  subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.5.1.1.3.2.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
     pf_compute1 = float(spl(pf_compute1))
 
-    pf_compute2 =  subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.9.10.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    pf_compute2 = float(spl(pf_compute2))
+    pf_compute2 = pf_compute1 # subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.9.10.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    pf_compute2 = pf_compute1 # float(spl(pf_compute2))
 
-    pf_compute3 =  subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.9.6.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    pf_compute3 = float(spl(pf_compute3))
+    pf_compute3 = pf_compute1 # subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.9.6.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    pf_compute3 = pf_compute1 # float(spl(pf_compute3))
 
-    pf_compute4 =  subprocess.Popen([r"snmpget","-v1","-c","public","10.150.1.93","iso.3.6.1.4.1.30966.7.1.9.21.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-    pf_compute4 = float(spl(pf_compute4))
+    pf_compute4 = pf_compute1 # subprocess.Popen([r"snmpget","-v1","-c","public",pdu_ip,"iso.3.6.1.4.1.30966.7.1.9.21.0"], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    pf_compute4 = pf_compute1 # float(spl(pf_compute4))
 
-    end = time.time() 
+    end = time.time()
 
     print('time takes: ', end-start)
     return (f'pdu_power{{compute_id="compute1"}} {round(curr_compute1*voltage_1_12, 3)}\n'
@@ -105,4 +141,3 @@ async def metrics():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8099)
-
